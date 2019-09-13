d20b9e4cc7fd0adb411036c1925a13369a80ee61
Raine Wallace <rainebeaudubs@gmail.com>
2019-09-13 13:08:58 -0400

    count_th is passing its tests

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,12 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
+  # must take in the 'word'
+  find_th = word.find('th')
+  # must see if and how many times it includes the letters "th" (case matters)
+  if find_th == -1:
+    # this means there were no "th"s, so return 0
+    return 0
+  # this means that if there are "th"s after the initial "th", to find them and add them up.
+  return 1 + count_th(word[find_th+2:])
     
-    pass
